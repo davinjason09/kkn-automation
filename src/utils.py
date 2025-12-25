1922616ae1402e3f366d2bcf9c5c23e06b589dd3
davinjason09 <davinjasonevanraharjo@mail.ugm.ac.id>
2025-12-25 09:02:14 +0700

    feat(utils): generate random coordinated in a certain radius

---
+++ src/utils.py
@@ -0,0 +1,23 @@
+import math
+import random
+
+
+def generate_random_points(lat: float, long: float, radius_m: int) -> tuple[str, str]:
+    earth_radius = 6378137.0  # (WGS84 spheroid)
+
+    random_dist = math.sqrt(random.random()) * radius_m
+    random_angle = 2 * math.pi * random.random()
+
+    delta_lat_rad = (random_dist / earth_radius) * math.sin(random_angle)
+    delta_long_rad = (random_dist / (earth_radius * math.cos(math.radians(lat)))) * math.cos(random_angle)
+
+    delta_lat_deg = math.degrees(delta_lat_rad)
+    delta_long_deg = math.degrees(delta_long_rad)
+
+    new_lat = lat + delta_lat_deg
+    new_long = long + delta_long_deg
+
+    new_lat = str(new_lat)[:7]
+    new_long = str(new_long)[:7]
+
+    return (new_lat, new_long)
